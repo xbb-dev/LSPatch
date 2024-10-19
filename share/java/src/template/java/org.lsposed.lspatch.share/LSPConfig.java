d02425e3585d1217af94179b9cc1a46591607078
xbb-dev <bangbangap@outlook.com>
2024-10-20 03:01:14 +0800

    Update LSPConfig.java

--- share/java/src/template/java/org.lsposed.lspatch.share/LSPConfig.java
+++ share/java/src/template/java/org.lsposed.lspatch.share/LSPConfig.java
@@ -15,10 +15,10 @@ public class LSPConfig {
 
     static {
         instance = new LSPConfig();
-        instance.API_CODE = ${apiCode};
-        instance.VERSION_CODE = ${verCode};
-        instance.VERSION_NAME = "${verName}";
-        instance.CORE_VERSION_CODE = ${coreVerCode};
-        instance.CORE_VERSION_NAME = "${coreVerName}";
+        instance.API_CODE = 93;
+        instance.VERSION_CODE = 398;
+        instance.VERSION_NAME = "0.6";
+        instance.CORE_VERSION_CODE = 7024;
+        instance.CORE_VERSION_NAME = "1.9.2";
     }
 }
